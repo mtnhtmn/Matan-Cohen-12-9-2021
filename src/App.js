dcbc3b7df7d50041cb6c6e6ffd98f26442677f80
mtnhtmn <mtnhtmn@gmail.com>
2021-09-20 03:47:04 +0300

    updating redux

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import HomePage from "./router/HomePage";
 import FavoritePage from "./router/FavoritePage";
 import Appbar from "./components/ui/Appbar";
 import Theme from "./components/ui/Theme";
+import {applyMiddleware, compose, createStore} from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import thunk from "redux-thunk";
+import {logger} from "redux-logger/src";
+import {Provider} from "react-redux";
 
+const store = createStore(rootReducer, compose(applyMiddleware(thunk, logger)))
 
 function App() {
     return (
-        <Router>
-            <Theme>
-                <Appbar/>
-                <Switch>
-                    <Route path={'/home'}>
-                        <HomePage/>
-                    </Route>
-                    <Route path={'/favorites'}>
-                        <FavoritePage/>
-                    </Route>
-                    <Redirect to={'/home'}/>
-                </Switch>
-            </Theme>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Theme>
+                    <Appbar/>
+                    <Switch>
+                        <Route path={'/home'}>
+                            <HomePage/>
+                        </Route>
+                        <Route path={'/favorites'}>
+                            <FavoritePage/>
+                        </Route>
+                        <Redirect to={'/home'}/>
+                    </Switch>
+                </Theme>
+            </Router>
+        </Provider>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
-import rootReducer from "./store/reducers/rootReducer";
-import {Provider} from "react-redux";
-import {logger} from "redux-logger/src";
-
-const store = createStore(rootReducer,
-    compose(
-        applyMiddleware(thunk, logger)
-    ))
-
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
